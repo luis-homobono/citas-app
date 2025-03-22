d43d32f425d03e15705b9ee2ff5503b2bcfa95f2
Luis Eloy Homobono Fuentes <eloy.homobono@gmail.com>
2025-03-22 13:10:11 -0600

    feat(auth): basic authentication
    refactor templates for DRY
    
    add url for login and logout

--- medical_appointments/urls.py
+++ medical_appointments/urls.py
@@ -5,6 +5,6 @@ from medical_appointments.views import home_page, booking_edit, booking_delete
 
 urlpatterns = [
     path('', home_page, name='booking'),
-    path('<int:id>', booking_edit),
-    path('delete/<int:id>', booking_delete),
+    path('<int:id>', booking_edit, name='edit'),
+    path('delete/<int:id>', booking_delete, name='delete'),
 ]
